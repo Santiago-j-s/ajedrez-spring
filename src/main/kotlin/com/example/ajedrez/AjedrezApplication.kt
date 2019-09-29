2766848c7653e4983473d5e1579daba4e393848c
Santiago Santana <santana.santiago@gmail.com>
2019-09-29 14:09:57 -0300

    First Commit

---
+++ src/main/kotlin/com/example/ajedrez/AjedrezApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ajedrez
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AjedrezApplication
+
+fun main(args: Array<String>) {
+	runApplication<AjedrezApplication>(*args)
+}
